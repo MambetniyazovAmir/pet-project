794a49c203c17d17d8c299016c81b808d1efcafa
MambetniyazovAmir <mr.amirniyaz@mail.ru>
2023-12-18 14:50:00 +0500

    brackets checking

--- code/index.php
+++ code/index.php
@@ -1,4 +1,25 @@
-<?php
-echo "Hello balancer nginx ";
+<html>
+<body>
 
-echo "Запрос обработал контейнер: " . $_SERVER['HOSTNAME'];
+<form action="" method="post">
+    Only brackets: <input type="text" name="name"><br>
+    <input type="submit">
+    <p>
+        <?php
+            $brackets = $_POST["name"];
+            $ans = 0;
+            for ($i=0; $i<strlen($brackets); $i++){
+                if($brackets[$i] == '(') $ans ++;
+                else $ans--;
+                if($ans < 0) {
+                    echo -1;
+                    return;
+                }
+            }
+            echo 1;
+        ?>
+    </p>
+</form>
+
+</body>
+</html>
